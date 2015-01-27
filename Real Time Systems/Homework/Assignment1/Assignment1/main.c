5d45813dc0c486619685144167852de4c3220efd
Mike Weispfenning <weis0292@umn.edu>
2015-01-26 20:30:53 -0600

    Original commit for real time systems homework assignment #1

---
+++ Real Time Systems/Homework/Assignment1/Assignment1/main.c
@@ -0,0 +1,43 @@
+/* Assignment1 - an application for the Pololu Orangutan SVP
+ *
+ * This application uses the Pololu AVR C/C++ Library.  For help, see:
+ * -User's guide: http://www.pololu.com/docs/0J20
+ * -Command reference: http://www.pololu.com/docs/0J18
+ *
+ * Created: 1/26/2015 8:25:18 PM
+ *  Author: Mike
+ */
+
+#include <pololu/orangutan.h>
+
+int main()
+{
+	clear();
+
+	while(1)
+	{
+		unsigned char button = wait_for_button_press(TOP_BUTTON | MIDDLE_BUTTON | BOTTOM_BUTTON);
+		
+		if(button == TOP_BUTTON)
+		{
+			print("Top Button");
+			green_led(HIGH);
+		}
+		else if(button == MIDDLE_BUTTON)
+		{
+			print("Middle Button");
+			green_led(HIGH);
+			red_led(HIGH);
+		}
+		else if(button == BOTTOM_BUTTON)
+		{
+			print("Button Button");
+			red_led(HIGH);
+		}
+		
+		wait_for_button_release(button);
+		green_led(LOW);
+		red_led(LOW);
+		clear();
+	}
+}
